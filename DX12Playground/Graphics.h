946994b8de717cfb12a99c6b53a72dc6e9d5c703
Ilddor <piotr.jerzy.koziol@gmail.com>
2016-04-25 21:13:19 +0200

    Init

---
+++ DX12Playground/Graphics.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <windows.h>
+
+#include "D3D.h"
+
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+class GraphicsClass
+{
+	D3DClass* m_Direct3D;
+
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+
+};
